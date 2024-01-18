9ae92429ca8a078fb58c22b9cb0577d3fddafe03
ryanleonardsecuriti <136494711+ryanleonardsecuriti@users.noreply.github.com>
2024-01-18 10:20:15 -0500

    Update script.js

--- Languages/script.js
+++ Languages/script.js
@@ -1,30 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the HTML tag
-    const htmlTag = document.querySelector('html');
+    // Get the button and content element
+    const translateButton = document.getElementById('translateButton');
+    const translatedContent = document.getElementById('translatedContent');
 
-    // Get the button
-    const languageButton = document.getElementById('languageButton');
-
-    // Set the URL for the language switch
-    const languageSwitchURL = "https://cdn-prod.securiti.ai/consent/cookie_banner/f849f6ee-3145-493e-a066-f4277dd87cb8/8bdb6a21-5691-411e-a1d0-96dd0ab85958/es.json";
+    // Set the URL for the translation JSON file
+    const translationURL = "https://cdn-prod.securiti.ai/consent/cookie_banner/f849f6ee-3145-493e-a066-f4277dd87cb8/8bdb6a21-5691-411e-a1d0-96dd0ab85958/it.json";
 
     // Function to fetch and apply translation
     function applyTranslation() {
         // Make an HTTP request to the specified URL
-        fetch(languageSwitchURL)
-            .then(response => response.json())
+        fetch(translationURL)
+            .then(response => {
+                // Check if the response status is OK (status code 200)
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(`Failed to fetch translation (${response.status} ${response.statusText})`);
+                }
+            })
             .then(data => {
                 // Update the content with the translated text
-                const translatedContent = document.getElementById('translatedContent');
-                translatedContent.textContent = data.translatedText;
+                translatedContent.textContent = data.translations;
             })
             .catch(error => {
-                console.error('Error fetching translation data:', error);
+                console.error('Error fetching or applying translation:', error);
             });
     }
 
     // Add click event listener to the button
-    languageButton.addEventListener('click', function() {
+    translateButton.addEventListener('click', function() {
         // Trigger the translation by calling the function
         applyTranslation();
     });
